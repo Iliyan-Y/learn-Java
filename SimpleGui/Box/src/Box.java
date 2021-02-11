f4450f79f97c18a9608e989a6a7a2092b2704548
Iliyan-Y <ili.yankov91@gmail.com>
2021-02-11 20:35:24 +0000

    Clickable button

--- SimpleGui/Box/src/Box.java
+++ SimpleGui/Box/src/Box.java
@@ -1,22 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Box {
+public class Box implements ActionListener {
+    private  int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
     public Box() {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+        panel = new JPanel();
+        JButton button = new JButton("Click me");
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: 0");
 
-        panel.setBorder(BorderFactory.createEmptyBorder(30,30,30,30));
+        panel.setBorder(BorderFactory.createEmptyBorder(30,30,80,120));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(label);
+        panel.add(button);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("BOxzs");
         frame.pack();
+
+
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
     new Box();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
     }
 }
