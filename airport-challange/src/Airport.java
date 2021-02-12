da7117da2b2621d4955afe2042ee76a54cbb72c5
Iliyan-Y <maximizebg@gmail.com>
2021-02-12 22:27:10 +0000

    Airport challange started

---
+++ airport-challange/src/Airport.java
@@ -0,0 +1,6 @@
+public class Airport {
+    public static void main(String[] args) {
+       Plane plane = new Plane("F1");
+        System.out.println(plane.name);
+    }
+}
---
+++ airport-challange/src/Plane.java
@@ -0,0 +1,8 @@
+public class Plane {
+    String name;
+
+    public Plane(String name) {
+        this.name = name;
+    }
+
+}
