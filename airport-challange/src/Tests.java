433756df4ed6e0d163485f928af22e1da0dc1a30
Iliyan-Y <maximizebg@gmail.com>
2021-02-13 11:55:04 +0000

    Test component refactured for better visual output

--- airport-challange/src/Tests.java
+++ airport-challange/src/Tests.java
@@ -1,33 +1,37 @@
 public class Tests {
+    static Plane plane1 = new Plane("F1");
+    static Airport ap1 = new Airport();
 
     public static void main(String[] args) {
-        try {
-            System.out.println("Testing: Planes can land at the airport");
-            Plane plane1 = new Plane("F1");
-            Airport ap1 = new Airport();
-            ap1.landPlane(plane1);
-
-            if(plane1.flying || !ap1.hangar.contains(plane1)){throw null;}else{
-                System.out.println("--Green--");
+        do {
+            int counter = 0;
+            try {
+                counter++;
+                System.out.println("Testing: Planes can land at the airport");
+                ap1.landPlane(plane1);
+                if(plane1.flying || !ap1.hangar.contains(plane1)){throw null;}else{
+                    System.out.println("Test: " + counter + " --Green--");
+                }
             }
-        }
-        catch (Exception e) {
-            System.out.println("----------------Something went wrong---------------");
-        }
-
-        try {
-            System.out.println("Testing: Planes can take off from the airport");
-            Plane plane1 = new Plane("F1");
-            Airport ap1 = new Airport();
-            ap1.landPlane(plane1);
-            ap1.takeOffPlane(plane1);
-
-            if(!plane1.flying || ap1.hangar.contains(plane1)){throw null;}else{
-                System.out.println("--Green--");
+            catch (Exception e) {
+                System.out.println("Test: " + counter + " -------Something went wrong-------");
+                break;
             }
-        }
-        catch (Exception e) {
-            System.out.println("----------------Something went wrong---------------");
-        }
+
+            try {
+                counter++;
+                System.out.println("Testing: Planes can take off from the airport");
+                ap1.takeOffPlane(plane1);
+                if(!plane1.flying || ap1.hangar.contains(plane1)){throw null;}else{
+                    System.out.println("Test: " + counter + " --Green--");
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Test: " + counter + " -------Something went wrong------");
+                break;
+            }
+
+            System.out.println(counter + " tests passed successfully");
+        } while (false);
     }
 }
