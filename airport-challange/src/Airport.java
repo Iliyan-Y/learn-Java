084bc3da501f19e05f8fe917fd8d006be6ceb5c4
Iliyan-Y <maximizebg@gmail.com>
2021-02-13 10:49:20 +0000

    AP class refactured to use List

--- airport-challange/src/Airport.java
+++ airport-challange/src/Airport.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Airport {
-    ArrayList<Plane> hangar;
-
-    public Airport() {
-        this.hangar  = new ArrayList<Plane>();
-    }
+    List<Plane> hangar = new ArrayList<>();
 
     public void landPlane(Plane plane) {
         hangar.add(plane);
     }
+
+    public String displayAllPlanes() {
+        List<String> planes = new ArrayList<>();
+        hangar.forEach(plane -> planes.add(plane.name));
+
+        return planes.toString();
+    }
+
 
 }
--- airport-challange/src/TowerControl.java
+++ airport-challange/src/TowerControl.java
@@ -1,11 +1,14 @@
 public class TowerControl {
+    static Plane plane1 = new Plane("F1");;
+    static Airport ap1 = new Airport();
+
 
     public static void main(String[] args) {
-        Plane plane1 = new Plane("F1");
-        Airport ap1 = new Airport();
         ap1.landPlane(plane1);
         ap1.landPlane(plane1);
+
         System.out.println(ap1.hangar.size());
         System.out.println(ap1.hangar.get(0).name);
+        System.out.println(ap1.displayAllPlanes());
     }
 }
