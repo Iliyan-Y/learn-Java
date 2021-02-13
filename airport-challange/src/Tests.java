f17848c9963170123a14afc7eafa66997607c15b
Iliyan-Y <maximizebg@gmail.com>
2021-02-13 11:35:06 +0000

    Second test: plane take off from airport

--- airport-challange/src/Tests.java
+++ airport-challange/src/Tests.java
@@ -7,7 +7,22 @@ public class Tests {
             Airport ap1 = new Airport();
             ap1.landPlane(plane1);
 
-            if(plane1.flying || ap1.hangar.size() == 0){throw null;}else{
+            if(plane1.flying || !ap1.hangar.contains(plane1)){throw null;}else{
+                System.out.println("--Green--");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("----------------Something went wrong---------------");
+        }
+
+        try {
+            System.out.println("Testing: Planes can take off from the airport");
+            Plane plane1 = new Plane("F1");
+            Airport ap1 = new Airport();
+            ap1.landPlane(plane1);
+            ap1.takeOffPlane(plane1);
+
+            if(!plane1.flying || ap1.hangar.contains(plane1)){throw null;}else{
                 System.out.println("--Green--");
             }
         }
