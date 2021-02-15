81001befa52318764920481f79ea577b956bf6df
Iliyan-Y <ili.yankov91@gmail.com>
2021-02-15 18:06:57 +0000

    Proper tests added with Junit5

---
+++ airport-challange/src/components/PlaneTest.java
@@ -0,0 +1,19 @@
+package components;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlaneTest {
+    @Test
+    void createPlaneWithName() {
+        Plane plane = new Plane("Test Plane");
+        assertEquals("Test Plane", plane.name);
+    }
+
+    @Test
+    void planeAlwaysFlyWhenCreated() {
+        Plane plane = new Plane("Test Plane");
+        assertEquals(true, plane.flying);
+    }
+}
