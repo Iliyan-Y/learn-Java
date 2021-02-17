adcd4beddd84154b7f65129ec48c37ccac85256b
Iliyan-Y <maximizebg@gmail.com>
2021-02-17 16:13:05 +0000

    Code refacture, challange compleated

--- airport-challange/src/components/Airport.java
+++ airport-challange/src/components/Airport.java
@@ -10,31 +10,13 @@ public class Airport {
   private boolean[] weather = {true, false};
 
   public void landPlane(Plane plane) {
-    if (hangar.size() == capacity) {
-      throw new IllegalArgumentException("airport is full");
-    }
-    if (checkWeather()) {
-      throw new IllegalArgumentException("Bad weather, airport is closed");
-    }
-    if (!plane.flying) {
-      throw new IllegalArgumentException("This plain isn't flying");
-    }
-
-    hangar.add(plane);
-    plane.flying = false;
-    System.out.println("Plane " + plane.name + " has landed successfully");
+    checkLandingProtocol(plane);
+    land(plane);
   }
 
   public void takeOffPlane(Plane plane) {
-    if (checkWeather()) {
-      throw new IllegalArgumentException("Bad weather, airport is closed");
-    }
-    if (!hangar.contains(plane)) {
-      throw new IllegalArgumentException("This plain isn't at this airport");
-    }
-    hangar.remove(plane);
-    plane.flying = true;
-    System.out.println("Plane " + plane.name + " has took off successfully");
+    checkTakeOffProtocol(plane);
+    takeOff(plane);
   }
 
   public void changeCapacity(int newCapacity) {
@@ -56,4 +38,36 @@ public class Airport {
     return weather[getRandomNumber()];
   }
 
+  private void land(Plane plane) {
+    hangar.add(plane);
+    plane.flying = false;
+    System.out.println("Plane " + plane.name + " has landed successfully");
+  }
+
+  private void takeOff(Plane plane) {
+    hangar.remove(plane);
+    plane.flying = true;
+    System.out.println("Plane " + plane.name + " has took off successfully");
+  }
+
+  private void checkLandingProtocol(Plane plane) {
+    if (hangar.size() == capacity) {
+      throw new IllegalArgumentException("airport is full");
+    }
+    if (checkWeather()) {
+      throw new IllegalArgumentException("Bad weather, airport is closed");
+    }
+    if (!plane.flying) {
+      throw new IllegalArgumentException("This plain isn't flying");
+    }
+  }
+
+  private void checkTakeOffProtocol(Plane plane) {
+    if (checkWeather()) {
+      throw new IllegalArgumentException("Bad weather, airport is closed");
+    }
+    if (!hangar.contains(plane)) {
+      throw new IllegalArgumentException("This plain isn't at this airport");
+    }
+  }
 }
